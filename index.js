666bf7f1123031d04c8273c7e40adc69d40109a7
Malissa Bishop <malissambishop7@gmail.com>
2021-09-29 11:39:50 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Malissa is the champion";
+
+document.body.append('h1');
